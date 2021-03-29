95295bd56680598feca0d1109fcdbbc1c53a07e4
Ivann <ivann@MacBook-Air-de-Ivann.local>
2021-03-29 19:13:22 +0200

    2nd commit of code

--- urbanSim.R
+++ urbanSim.R
@@ -5,6 +5,13 @@ library(tmap)
 library(tmaptools)
 library(igraph)
 
-
-
 #### Reading DATA ####
+
+tube <- st_read("Data/underground/underground.shp")
+
+
+#### Overview of data  ####
+
+tmap_mode("view")
+
+qtm(tube)
